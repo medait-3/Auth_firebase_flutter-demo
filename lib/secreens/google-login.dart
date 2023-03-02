8f9e6c26f85cbe5ecec88e73de8cfc439f484fa0
meait-3 <tchikh3@@gmail.com>
2023-03-02 17:08:00 +0100

    google sign

---
+++ lib/secreens/google-login.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Google_login extends StatefulWidget {
+  const Google_login({super.key});
+
+  @override
+  State<Google_login> createState() => _Google_loginState();
+}
+
+class _Google_loginState extends State<Google_login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
